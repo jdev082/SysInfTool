3cf0771c7412a7dc992efcb92aa1ca33a3795842
jdev082 <jdev082@jdev.eu.org>
2023-06-10 18:32:41 -0500

    feat: add installation date

--- content.py
+++ content.py
@@ -4,6 +4,7 @@ import platform
 import psutil
 
 cpu = os.popen("cat /proc/cpuinfo | grep 'model name' | cut -c14- | sed -n 1p").read()
+instdat = os.popen("stat / | grep 'Birth' | sed 's/Birth: //g' | cut -b 2-11").read()
 content = f"""
 <h1>{distro.name()} {distro.version()}</h1>
 <h2>Userspace</h2>
@@ -13,6 +14,7 @@ content = f"""
 <p>session type: {os.getenv('XDG_SESSION_TYPE')}
 <h2>System Software Components</h2>
 <p>kernel: {platform.release()}</p>
+<p>installation date: {instdat}</p>
 <h2>Computer Hardware</h2>
 <p>CPU: {cpu}
 <p>RAM: {str(psutil.virtual_memory().total / (1024 * 1024 * 1024))[0:4]} GB""" 
