d0eb7ea5c66400253fffece9f52a7fc2fc79295f
jdev082 <jdev082@jdev.eu.org>
2023-06-10 18:49:54 -0500

    feat: add terminal output arg

--- main.py
+++ main.py
@@ -13,6 +13,21 @@ from functions import *
 from appmeta import *
 from content import *
 
+# Arguments
+
+if len(sys.argv) > 1:
+    if sys.argv[1] == '-o' or sys.argv[1] == '--out':
+        content = content.replace("<h1>", "\033[1m")
+        content = content.replace("</h1>", "\033[0m")
+        content = content.replace("<h2>", "\033[1m")
+        content = content.replace("</h2>", "\033[0m")
+        content = content.replace("<p>", " ")
+        content = content.replace("</p>", " ")
+        print(content)
+        exit()
+    if sys.argv[1] == 'h' or sys.argv[1] == '--help':
+        print('./main.py (-o/--out output in terminal)')
+
 # Application
 app = QApplication([])
 
